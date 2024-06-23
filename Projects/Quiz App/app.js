6f8cac5887237fee43678950c206fbbe2b9afce4
sh-webwork <sh.official007@gmail.com>
2024-06-23 12:35:07 +0500

    updated 23-06-2024

--- Projects/Quiz App/app.js
+++ Projects/Quiz App/app.js
@@ -1,4 +1,90 @@
-function formSubmitHandler(){
+var startQuiz = document.getElementById("startQuiz")
+var myContainer = document.getElementById("myContainer")
+var username = document.getElementById("username")
+var email = document.getElementById("email")
 
-    console.log("hi world")
+function formLoginHandler() {
+
+    var password = document.getElementById("password")
+    var confirmPassword = document.getElementById("confirm-password")
+    // console.log("myContainer", myContainer.className)
+
+    
+    // if (password.value == confirmPassword.value) {
+        
+        myContainer.className = "hide"
+        startQuiz.className = "show"
+        localStorage.setItem("username", username.value)
+        localStorage.setItem("email", email.value)
+        // console.log("local storage data: ", localStorage.value)
+        // return
+    // } 
+    // else {
+
+    //     alert("password mismatched...")
+    // }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("Hello World!!");
+// var formContainer = document.getElementById("formContainer")
+// var startContainer = document.getElementById("startContainer")
+
+
+// function formSubmitHandler() {
+//     var name = document.getElementById("name")
+//     var email = document.getElementById("email")
+//     var nameError = document.getElementById("nameError")
+//     var emailError = document.getElementById("emailError")
+
+//     // localStorage.setItem("name", name.value)  //set item
+//     // console.log(localStorage.getItem("name")) //get item
+//     // localStorage.removeItem("name") // remove item
+//     nameError.className = "hide"
+//     emailError.className = "hide"
+//     if (!name.value) {
+//         nameError.className = "show"
+//         nameError.style.color = "red"
+//         return
+//     }
+
+//     if (!email.value) {
+//         emailError.className = "show"
+//         emailError.style.color = "red"
+//         return
+//     }
+
+//     localStorage.setItem("name", name.value)
+//     localStorage.setItem("email", email.value)
+//     formContainer.className = "hide"
+//     startContainer.className = "show"
+//     // console.log("form submitted!")
+
+// }
